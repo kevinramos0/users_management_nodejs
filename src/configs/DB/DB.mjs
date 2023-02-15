29eb6a73dec803c9360764754f4d19b12be71911
kevin.ramosb <kevin.ramosb@salud.gob.sv>
2023-02-15 07:38:14 -0600

    no show queries sql in terminal

--- src/configs/DB/DB.mjs
+++ src/configs/DB/DB.mjs
@@ -16,6 +16,7 @@ export default class db {
         host: dbConfig.options.db_host,
         port: dbConfig.options.db_port,
         dialect: dbConfig.motor,
+        logging: false,
       }
     );
   }
